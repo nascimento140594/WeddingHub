6b002ced9b8c717f9712a7559d9e8709de34903c
Matheus Araujo Nascimento <nascimento140594@gmail.com>
2026-07-30 20:10:01 -0300

    feat: add filtering, search and ordering for guestbook

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import GuestMessage
@@ -6,6 +9,44 @@ from .serializers import GuestMessageSerializer
 
 
 class GuestMessageViewSet(viewsets.ModelViewSet):
-    queryset = GuestMessage.objects.all()
     serializer_class = GuestMessageSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = GuestMessage.objects.select_related("wedding")
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "approved",
+    ]
+
+    search_fields = [
+        "guest_name",
+        "guest_email",
+        "message",
+    ]
+
+    ordering_fields = [
+        "guest_name",
+        "created_at",
+    ]
+
+    ordering = [
+        "-created_at",
+    ]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.request.user.is_authenticated:
+            return queryset.filter(
+                wedding__owner=self.request.user,
+            )
+
+        return queryset.filter(
+            approved=True,
+        )
