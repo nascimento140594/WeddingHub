39e5c133ee47ba33be8b66b046b10c3cadb4ef40
Matheus Araujo Nascimento <nascimento140594@gmail.com>
2026-07-29 21:37:25 -0300

    feat: add filtering, search and ordering for gifts

--- gifts/views.py
+++ gifts/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Gift
@@ -9,11 +12,39 @@ class GiftViewSet(viewsets.ModelViewSet):
     serializer_class = GiftSerializer
     permission_classes = [IsAuthenticated]
 
+    queryset = Gift.objects.select_related("wedding")
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "reserved",
+    ]
+
+    search_fields = [
+        "name",
+        "description",
+    ]
+
+    ordering_fields = [
+        "name",
+        "price",
+        "created_at",
+    ]
+
+    ordering = [
+        "name",
+    ]
+
     def get_queryset(self):
-        return Gift.objects.filter(
-            wedding__owner=self.request.user
+        return self.queryset.filter(
+            wedding__owner=self.request.user,
         )
 
     def perform_create(self, serializer):
-        wedding = self.request.user.wedding
-        serializer.save(wedding=wedding)
+        serializer.save(
+            wedding=self.request.user.wedding,
+        )
