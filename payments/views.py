5f184cd1484c71f07289d6895ed9aa509b5d28d9
Matheus Araujo Nascimento <nascimento140594@gmail.com>
2026-07-29 21:40:56 -0300

    feat: add filtering, search and ordering for payments

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Payment
@@ -9,7 +12,39 @@ class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     permission_classes = [IsAuthenticated]
 
+    queryset = Payment.objects.select_related(
+        "gift",
+        "gift__wedding",
+    )
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "status",
+    ]
+
+    search_fields = [
+        "guest_name",
+        "guest_email",
+        "transaction_id",
+    ]
+
+    ordering_fields = [
+        "amount",
+        "created_at",
+        "guest_name",
+        "status",
+    ]
+
+    ordering = [
+        "-created_at",
+    ]
+
     def get_queryset(self):
-        return Payment.objects.filter(
+        return self.queryset.filter(
             gift__wedding__owner=self.request.user
         )
